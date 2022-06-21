9f2e114a145ed9186a805768e75871aeafcc31ce
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-21 18:05:39 +0800

    Create GameView.java

---
+++ app/src/main/java/nero/hua/a2048forandroid/GameView.java
@@ -0,0 +1,27 @@
+package nero.hua.a2048forandroid;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridLayout;
+
+public class GameView extends GridLayout {
+    public GameView(Context context) {
+        super(context);
+        initGameView();
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initGameView();
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initGameView();
+    }
+
+    private void initGameView() {
+
+    }
+
+}
