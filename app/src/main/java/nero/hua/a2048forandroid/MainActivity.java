512c381a020dbe40507f55ea7b7f53b2f60bc8ce
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-21 16:48:59 +0800

    Create MainActivity.java

---
+++ app/src/main/java/nero/hua/a2048forandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package nero.hua.a2048forandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
