a19995018ecf0b9878b38c5494d56da40c769a8b
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-22 16:20:23 +0800

    Update MainActivity.java

--- app/src/main/java/nero/hua/a2048forandroid/MainActivity.java
+++ app/src/main/java/nero/hua/a2048forandroid/MainActivity.java
@@ -1,13 +1,63 @@
 package nero.hua.a2048forandroid;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.GridLayout;
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class GameView extends GridLayout {
+    public GameView(Context context) {
+        super(context);
+        initGameView();
     }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initGameView();
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initGameView();
+    }
+
+    private void initGameView() {
+        setOnTouchListener(new OnTouchListener() {
+
+            private float startX, startY, offsetX, offsetY;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (MotionEvent.ACTION_DOWN == motionEvent.getAction()) {
+                    startX = motionEvent.getX();
+                    startY = motionEvent.getY();
+                }
+                else if (MotionEvent.ACTION_DOWN == motionEvent.getAction()) {
+                    offsetX = motionEvent.getX() - offsetX;
+                    offsetY = motionEvent.getY() - offsetY;
+
+                    if (Math.abs(offsetX) >= Math.abs(offsetY)) {
+                        if (offsetX < -5) {
+                            System.out.println("left");
+                        }
+                        else if (offsetX > 5) {
+                            System.out.println("right");
+                        }
+                    }
+                    else {
+                        if (offsetY < -5) {
+                            System.out.println("left");
+                        }
+                        else if (offsetY > 5) {
+                            System.out.println("right");
+                        }
+                    }
+                }
+                return true;
+            }
+
+        });
+    }
+
 }
