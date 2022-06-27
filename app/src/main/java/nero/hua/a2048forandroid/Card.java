b71d526f0eaab5e5d4f812aeacd052eed2ed6f38
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-27 11:53:20 +0800

    Update Card.java

--- app/src/main/java/nero/hua/a2048forandroid/Card.java
+++ app/src/main/java/nero/hua/a2048forandroid/Card.java
@@ -1,6 +1,7 @@
 package nero.hua.a2048forandroid;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -13,6 +14,17 @@ public class Card extends FrameLayout {
 
     public Card(@NonNull Context context) {
         super(context);
+
+        label = new TextView(getContext());
+        label.setTextSize(32);
+        label.setGravity(Gravity.CENTER);
+        label.setBackgroundColor(0x33FFFFFF);
+
+        LayoutParams layoutParams = new LayoutParams(-1, -1);
+        layoutParams.setMargins(10, 10, 0, 0);
+        addView(label, layoutParams);
+
+        setNumberAndLabel(0);
     }
 
     public int getNumber() {
@@ -25,11 +37,25 @@ public class Card extends FrameLayout {
 
     public void setNumberAndLabel(int number) {
         this.number = number;
-        label.setText(number + "");
+
+        if (number > 0) {
+            label.setText(number + "");
+        }
+        else {
+            label.setText("");
+        }
     }
 
     public boolean equals(Card card) {
         return null == card ? false : this.number == card.getNumber();
+    }
+
+    public boolean isEmpty() {
+        return this.number <= 0;
+    }
+
+    public boolean isNotEmpty() {
+        return this.number > 0;
     }
 
 }
