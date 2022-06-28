2d90f99acf4ee4a2e06fc3d08ea73f645440b3e6
neroHua <38644203+neroHua@users.noreply.github.com>
2022-06-28 16:05:02 +0800

    Create GameConfig.java

---
+++ app/src/main/java/nero/hua/a2048forandroid/GameConfig.java
@@ -0,0 +1,13 @@
+package nero.hua.a2048forandroid;
+
+public class GameConfig {
+
+    public static int CARD_COUNT = 4;
+
+    public static int CARD_GAP = 10;
+    public static int CARD_WIDTH = 300;
+    public static double PROBABILITY_FOR_2 = 0.9;
+    public static int RANDOM_2 = 2;
+    public static int RANDOM_4 = 4;
+
+}
